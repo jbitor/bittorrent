401c102a35be3a65fb51ed337130fcda93de4490
Jeremy Banks <j@jeremybanks.ca>
2015-07-03 20:51:11 -0700

    work towards listening for incoming peer connections

--- swarm.go
+++ swarm.go
@@ -11,16 +11,29 @@ import (
 	"github.com/jbitor/bencoding"
 )
 
-type Swarm interface {
-	// The infohash of the torrent.
-	Id() (infoHash BTID)
+type Client interface {
+	// The peer ID
+	Id() BTID
 
+	// local TCP binding?
+
+	// Returns a Swarm for the specified infohash, creating one if it doesn't
+	// exist.
+	Swarm(infohash BTID)
+}
+
+type Swarm interface {
+	// The infohash of the torrent this swarm exists for
+	Id() BTID
+	// Client() Client
 	// Returns the torrent's info dictionary. Blocks until
 	// it's been downloaded from peers.
 	GetInfo() (info *bencoding.Dict)
 
 	AddPeer(peer *RemotePeer)
 }
+
+const PORT = 6881
 
 type swarm struct {
 	infoHash BTID
@@ -58,6 +71,145 @@ func (s *swarm) GetInfo() (info *bencoding.Dict) {
 
 	wg.Add(1)
 
+	listener, err := net.ListenTCP("tcp", &net.TCPAddr{Port: PORT})
+	if err != nil {
+		logger.Fatalf("unable to listen for peer connections: %v", err)
+	}
+
+	// Handles a connection to a peer, either incoming or outgoing
+	handleConnection := func(conn *net.TCPConn, peer *RemotePeer) {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			logger.Printf("Sending handshake to %v", peer)
+			writeHandshake(conn, peerId, s.infoHash)
+
+			// Send keepalives every couple minutes
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for {
+					logger.Printf("Sending keepalive to %v", peer)
+					writeKeepAlive(conn)
+					time.Sleep(120 * time.Second)
+				}
+			}()
+
+			// Read incoming messages
+			wg.Add(1)
+			go func() {
+				length := int32(0)
+				defer wg.Done()
+
+				gotHandshake := false
+
+				handleChunk := func(chunk string) {
+					// XXX: This is treating a chunk as a full message. Very bad.
+					logger.Printf("got chunk from %v, length %v", peer, len(chunk))
+
+					if !gotHandshake {
+						if chunk[0:len(peerProtocolHeader)] != peerProtocolHeader {
+							logger.Printf("Peer protocol header missing!")
+						}
+
+						gotHandshake = true
+
+						logger.Printf("got handshake: %v", chunk)
+					} else {
+						if len(chunk) < 4 {
+							logger.Printf("ignoring too-short chunk")
+							return
+						}
+						buf := bytes.NewBuffer([]byte(chunk[0:4]))
+						binary.Read(buf, binary.BigEndian, &length)
+						logger.Printf("message length: %v", length)
+
+						if length < 0 {
+							logger.Printf("got -length message...")
+							return
+						} else if length == 0 {
+							logger.Printf("Got keepalive.")
+							return
+						} else if int32(len(chunk))-4 < length {
+							logger.Printf("Ignoring too-long (chunked?) message")
+							return
+						}
+
+						msgType := messageType(chunk[4])
+						body := chunk[5 : 5+length-1]
+
+						logger.Printf("got message type/body: %v/%v", msgType, body)
+
+						switch msgType {
+						case msgExtended:
+							extendedType := body[0]
+							if extendedType != 0 {
+								logger.Printf("Got unsupported extension message type: %v", extendedType)
+								return
+							}
+							bencoded := body[1:len(body)]
+							logger.Printf("Got an extension handshake message")
+							data, err := bencoding.Decode([]byte(bencoded))
+
+							if err != nil {
+								logger.Printf("Error decoding message: %v", err)
+								return
+							}
+
+							// Check if the peer supports metadata exchange
+							if dataM, hasM := data.(bencoding.Dict)["m"]; hasM {
+								if mdxId, hasMdx := dataM.(bencoding.Dict)["ut_metadata"]; hasMdx {
+									logger.Printf("Peer %v supports metadata exchange, using extension ID %v.", peer, mdxId)
+								} else {
+									logger.Printf("Peer %v does not support metadata exchange!", peer)
+									return
+								}
+							} else {
+								logger.Printf("Peer %v does not support any extensions!", peer)
+								return
+							}
+
+						default:
+							logger.Printf("Got unsupported message type: %v", msgType)
+						}
+					}
+				}
+
+				buffer := make([]byte, 32768)
+				for {
+					readLength, err := conn.Read(buffer)
+
+					if err != nil && err != io.EOF {
+						logger.Printf("Error reading from %v: %v", peer, err)
+						time.Sleep(6 * time.Second)
+					} else if readLength > 0 {
+						handleChunk(string(buffer[0:readLength]))
+					} else {
+						logger.Printf("Got zero-length no-error read() from %v", peer)
+						time.Sleep(6 * time.Second)
+					}
+				}
+			}()
+		}()
+	}
+
+	// Listen for incoming connections
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		for {
+			conn, err := listener.AcceptTCP()
+			if err != nil {
+				logger.Printf("Error accepting new connection: %v", err)
+			} else {
+				handleConnection(conn, nil)
+			}
+		}
+	}()
+
+	// Connect to all known peers
 	for _, peer := range s.peers {
 		func(peer *RemotePeer) {
 			wg.Add(1)
@@ -66,110 +218,21 @@ func (s *swarm) GetInfo() (info *bencoding.Dict) {
 
 				logger.Printf("Connecting to peer %v.\n", peer)
 
+				// TODO(jre): We really need to dial on the same port as we're
+				// listening, but net doesn't directly let us do that.
 				conn, err := net.DialTCP("tcp", nil, &peer.Address)
 				if err != nil {
 					logger.Printf("Failed to connect to %v: %v", peer, err)
 					return
 				}
-				// conn.Close()
 
 				logger.Printf("Successfully connected to peer %v.\n", peer)
 
-				logger.Printf("Sending handshake to %v", peer)
-				writeHandshake(conn, peerId, s.infoHash)
-
-				// Send keepalives every couple minutes
-				wg.Add(1)
-				go func() {
-					defer wg.Done()
-					for {
-						logger.Printf("Sending keepalive to %v", peer)
-						writeKeepAlive(conn)
-						time.Sleep(120 * time.Second)
-					}
-				}()
-
-				// Read incoming messages
-				wg.Add(1)
-				go func() {
-					length := int32(0)
-					defer wg.Done()
-
-					gotHandshake := false
-
-					handleChunk := func(chunk string) {
-						// XXX: This is treating a chunk as a full message. Very bad.
-						logger.Printf("got chunk from %v, length %v", peer, len(chunk))
-
-						if !gotHandshake {
-							if chunk[0:len(peerProtocolHeader)] != peerProtocolHeader {
-								logger.Printf("Peer protocol header missing!")
-							}
-
-							gotHandshake = true
-
-							logger.Printf("got handshake: %v", chunk)
-						} else {
-							if len(chunk) < 4 {
-								logger.Printf("ignoring too-short chunk")
-								return
-							}
-							buf := bytes.NewBuffer([]byte(chunk[0:4]))
-							binary.Read(buf, binary.BigEndian, &length)
-							logger.Printf("message length: %v", length)
-
-							if length < 0 {
-								logger.Printf("got -length message...")
-								return
-							} else if length == 0 {
-								logger.Printf("Got keepalive.")
-								return
-							} else if int32(len(chunk))-4 < length {
-								logger.Printf("Ignoring too-long (chunked?) message")
-								return
-							}
-
-							msgType := messageType(chunk[4])
-							body := chunk[5 : 5+length-1]
-
-							logger.Printf("got message type/body: %v/%v", msgType, body)
-
-							switch msgType {
-							case msgExtended:
-								extendedType := body[0]
-								if extendedType != 0 {
-									logger.Printf("Got unsupported extension message type: %v", extendedType)
-									return
-								}
-								bencoded := body[1:len(body)]
-								logger.Printf("Got an extension handshake message")
-								data, err := bencoding.Decode([]byte(bencoded))
-								logger.Printf("error/message: %v/%v", err, data)
-							default:
-								logger.Printf("Got unsupported message type: %v", msgType)
-							}
-						}
-					}
-
-					buffer := make([]byte, 32768)
-					for {
-						readLength, err := conn.Read(buffer)
-
-						if err != nil && err != io.EOF {
-							logger.Printf("Error reading from %v: %v", peer, err)
-							time.Sleep(6 * time.Second)
-						} else if readLength > 0 {
-							handleChunk(string(buffer[0:readLength]))
-						} else {
-							logger.Printf("Got zero-length no-error read() from %v", peer)
-							time.Sleep(6 * time.Second)
-						}
-					}
-				}()
+				handleConnection(conn, peer)
 			}()
 		}(peer)
 
-		time.Sleep(1 * time.Second)
+		time.Sleep(4 * time.Second)
 	}
 
 	wg.Done()
